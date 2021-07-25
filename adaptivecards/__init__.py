9984459ddd3b9c290a1045a7221d079c5960886c
Nguyen Huu Hoa <huuhoa@gmail.com>
2021-07-26 06:03:43 +0700

    chore: change string format

--- adaptivecards/__init__.py
+++ adaptivecards/__init__.py
@@ -23,7 +23,7 @@ SOFTWARE.
 
 __author__ = """Huu Hoa NGUYEN"""
 __email__ = "huuhoa@gmail.com"
-__version__ = "0.1.0"
+__version__ = '0.1.0'
 __copyright__ = "Copyright (c) 2021 Huu Hoa NGUYEN"
 __license__ = "MIT License"
 
